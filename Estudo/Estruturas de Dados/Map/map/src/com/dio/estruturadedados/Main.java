3cda4276567f89e1d25986b7fb19eafae01337bf
paulohenr96 <paulohrq_96@hotmail.com>
2023-03-14 15:31:31 -0300

    Mapas

---
+++ Estudo/Estruturas de Dados/Map/map/src/com/dio/estruturadedados/Main.java
@@ -0,0 +1,42 @@
+package com.dio.estruturadedados;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Map < String, String> aluno = new HashMap<>();
+	
+	aluno.put("Nome", "João");
+	aluno.put("Idade", "17");
+	aluno.put("Media", "8.5");
+	aluno.put("Turma", "3a");
+
+	
+	System.out.println(aluno);
+	
+	System.out.println(aluno.keySet());
+	System.out.println(aluno.values());
+
+	List<Map < String, String>> listaAlunos = new ArrayList<Map<String,String>>();
+	
+	listaAlunos.add(aluno);
+	
+Map < String, String> aluno2 = new HashMap<>();
+	
+	aluno.put("Nome", "Claudio");
+	aluno.put("Idade", "16");
+	aluno.put("Media", "5");
+	aluno.put("Turma", "3b");
+	listaAlunos.add(aluno2);
+
+	System.out.println(listaAlunos);
+
+	}
+
+}
